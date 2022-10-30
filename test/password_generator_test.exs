7d2e0fe00b37894fd3cda7ef77c165248d1db8f0
david.g <david.g@kakaopaysec.com>
2022-10-30 23:25:13 +0900

    init commit

---
+++ test/password_generator_test.exs
@@ -0,0 +1,31 @@
+defmodule PasswordGeneratorTest do
+  use ExUnit.Case
+  doctest PasswordGenerator
+
+  setup do
+    options = %{
+      "length" => "10",
+      "numbers" => "false",
+      "uppercase" => "false",
+      "symbols" => "false"
+    }
+
+    options_type = %{
+      lowercase: Enum.map(?a..?z, &<<&1>>),
+      numbers: Enum.map(0..9, &Integer.to_string(&1)),
+      uppercase: Enum.map(?A..?Z, &<<&1>>),
+      symbols: String.split("!#$%&()*+,-.:;<=>?@[]^_{|}~", "", trim: true)
+    }
+
+    {:ok, result} = PasswordGenerator.generate(options)
+
+    %{
+      options_type: options_type,
+      result: result
+    }
+  end
+
+  test "retuns a string", %{result: result} do
+    assert is_bitstring(result)
+  end
+end
