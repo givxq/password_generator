360425da00aad78d7ff7731c7e3e647b4123eecd
david.g <david.g@kakaopaysec.com>
2022-10-30 23:38:53 +0900

    test code 작성

--- test/password_generator_test.exs
+++ test/password_generator_test.exs
@@ -28,4 +28,57 @@ defmodule PasswordGeneratorTest do
   test "retuns a string", %{result: result} do
     assert is_bitstring(result)
   end
+
+  test "returns error when no length is given" do
+    options = %{"invalid" => "false"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "returns error when length is not an integer" do
+    options = %{"length" => "ab"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "length of returned string is the option provided" do
+    length_option = %{"length" => "5"}
+    {:ok, result} = PasswordGenerator.generate(length_option)
+
+    assert 5 = String.length(result)
+  end
+
+  test "return a lowercase string just with the length", %{options_type: options} do
+    length_option = %{"length" => "5"}
+    {:ok, result} = PasswordGenerator.generate(length_option)
+
+    assert String.contains?(result, options.lowercase)
+
+    refute String.contains?(result, options.numbers)
+    refute String.contains?(result, options.numbers)
+    refute String.contains?(result, options.numbers)
+  end
+
+  test "returns error when options values are not booleans" do
+    options = %{
+      "length" => "10",
+      "numbers" => "invalid",
+      "uppercase" => "0",
+      "symbols" => "false"
+    }
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "returns error when options not allowed" do
+    options = %{"length" => "5", "invalid" => "true"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "return error when 1 option not allowed" do
+    options = %{"length" => "5", "numbers" => "true", "invalid" => "true"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
 end
